8709f1856ad2b7c68b535a7dbdb0b9f886726e35
WagnerMessias <by.wagnermc@gmail.com>
2018-04-23 22:45:18 -0300

    Initial commit

---
+++ app/src/main/java/br/eti/wagnermessias/marvelexample/characters/CharactersActivity.java
@@ -0,0 +1,15 @@
+package br.eti.wagnermessias.marvelexample.characters;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.eti.wagnermessias.marvelexample.R;
+
+public class CharactersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_characters);
+    }
+}
---
+++ app/src/main/java/br/eti/wagnermessias/marvelexample/comics/empty.java
@@ -0,0 +1,8 @@
+package br.eti.wagnermessias.marvelexample.comics;
+
+/**
+ * Created by Wagner on 23/04/2018.
+ */
+
+public class empty {
+}
