742cbb8b24ebc3c4dc5cb8a18957a6bf45079738
WagnerMessias <by.wagnermc@gmail.com>
2018-04-24 07:51:10 -0300

    Implementando Recycler

--- app/src/main/java/br/eti/wagnermessias/marvelexample/characters/CharactersActivity.java
+++ app/src/main/java/br/eti/wagnermessias/marvelexample/characters/CharactersActivity.java
@@ -2,14 +2,45 @@ package br.eti.wagnermessias.marvelexample.characters;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
 
 import br.eti.wagnermessias.marvelexample.R;
+import br.eti.wagnermessias.marvelexample.entities.Character;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class CharactersActivity extends AppCompatActivity {
+
+    @BindView(R.id.rv_characters)
+    RecyclerView mRecyclerView;
+    private List<Character> characters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_characters);
+        ButterKnife.bind(this);
+
+//        for (int i = 1; i <= 10;i++ ) {
+//            characters.add(new Character("Spider-Man", "http://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b.jpg"));
+//        }
+
+        initRecyclerView(characters);
+    }
+
+//    https://www.journaldev.com/13792/android-gridlayoutmanager-example
+    public void initRecyclerView(List<Character> characters) {
+        //btn_tray_again.setVisibility(View.GONE);
+        this.characters = characters;
+        mRecyclerView.setHasFixedSize(true);
+//        layoutManager = new LinearLayoutManager(this);
+//        mRecyclerView.setLayoutManager(layoutManager);
+//        adapter = new RepositoriesAdapter(this, this.repositories);
+//        adapter.setClickListener(this);
+//        mRecyclerView.setAdapter(adapter);
+//        mRecyclerView.addOnScrollListener(createInfiniteScrollListener());
+//        showLoadingRecyclerView(false);
     }
 }
---
+++ app/src/main/java/br/eti/wagnermessias/marvelexample/entities/Character.java
@@ -0,0 +1,16 @@
+package br.eti.wagnermessias.marvelexample.entities;
+
+/**
+ * Created by Wagner on 23/04/2018.
+ */
+
+public class Character {
+
+    private String name;
+    private String imagem;
+
+    public Character(String name, String imagem) {
+        this.name = name;
+        this.imagem = imagem;
+    }
+}
