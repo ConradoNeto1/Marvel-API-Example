9fdfbf4e56e7632f74330784fbf1141f8a344cde
WagnerMessias <by.wagnermc@gmail.com>
2018-04-30 07:04:16 -0300

    Mais ajustes

---
+++ app/src/main/java/br/eti/wagnermessias/marvelexample/helpers/Constants.java
@@ -0,0 +1,13 @@
+package br.eti.wagnermessias.marvelexample.helpers;
+
+/**
+ * Created by Wagner on 29/04/2018.
+ */
+
+public class Constants {
+
+    public static String PRIVATE_KEY = "e4664e13d7dfb4323f5a77b3ec04a039";
+    public static String PUBLIC_KEY = "c3871b2cc6382a43341545f7224b89b13a71c5d6";
+    public static String BASE_ENDPOINT = "https://gateway.marvel.com";
+
+}
---
+++ app/src/main/java/br/eti/wagnermessias/marvelexample/helpers/Util.java
@@ -0,0 +1,18 @@
+package br.eti.wagnermessias.marvelexample.helpers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Wagner on 29/04/2018.
+ */
+
+public class Util {
+
+    public static List<String> getAuthorizationParameterValues(){
+        List<String> parametersValues = new ArrayList<String>();
+
+
+return null;
+    }
+}
